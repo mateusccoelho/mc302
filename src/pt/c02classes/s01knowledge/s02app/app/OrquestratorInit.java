b81adedaedf8f062cc2731152374602865abc1c2
Mateus Coelho <mateus_coelho@live.com>
2015-03-14 18:29:18 -0300

    Criado módulo principal OrquestratorInit

---
+++ src/pt/c02classes/s01knowledge/s02app/app/OrquestratorInit.java
@@ -0,0 +1,63 @@
+package pt.c02classes.s01knowledge.s02app.app;
+
+import pt.c02classes.s01knowledge.s01base.impl.BaseConhecimento;
+import pt.c02classes.s01knowledge.s01base.impl.Statistics;
+import pt.c02classes.s01knowledge.s01base.inter.IBaseConhecimento;
+import pt.c02classes.s01knowledge.s01base.inter.IEnquirer;
+import pt.c02classes.s01knowledge.s01base.inter.IResponder;
+import pt.c02classes.s01knowledge.s01base.inter.IStatistics;
+import pt.c02classes.s01knowledge.s02app.actors.EnquirerMaze;
+import pt.c02classes.s01knowledge.s02app.actors.ResponderMaze;
+import pt.c02classes.s01knowledge.s02app.actors.EnquirerAnimals;
+import pt.c02classes.s01knowledge.s02app.actors.ResponderAnimals;
+
+import java.util.Scanner;
+
+public class OrquestratorInit {
+
+	public static void main(String[] args) {
+		
+		IEnquirer enq;
+		IResponder resp;
+		IStatistics stat = new Statistics();
+		IBaseConhecimento base = new BaseConhecimento();
+		Scanner entrada = new Scanner(System.in);
+		String jogo = "Nada";
+		
+		while (!jogo.equalsIgnoreCase("A") && !jogo.equalsIgnoreCase("M")) {
+			System.out.println("Deseja jogar (M)aze ou (A)nimals?");
+			jogo = entrada.nextLine();
+		}
+		
+		if(jogo.equalsIgnoreCase("A")) {
+			base.setScenario("animals");
+			enq = new EnquirerAnimals();
+			String listaAnimais[] = base.listaNomes();
+			System.out.println("Digite o numero correspondente ao animal que deseja jogar.");
+			int i;
+			for(i = 0; i < listaAnimais.length; i++)
+				System.out.println(i + " - " + listaAnimais[i]);
+			i = entrada.nextInt();
+			System.out.println("Enquirer com " + listaAnimais[i] + "...");
+			resp = new ResponderAnimals(stat, listaAnimais[i]);
+			enq.connect(resp);
+			enq.discover();
+		}
+		else {
+			base.setScenario("maze");
+			enq = new EnquirerMaze();
+			String listaMapas[] = base.listaNomes();
+			System.out.println("Digite o numero correspondente ao labirinto que deseja jogar.");
+			int i;
+			for(i = 0; i < listaMapas.length; i++)
+				System.out.println(i + " - " + listaMapas[i]);
+			i = entrada.nextInt();
+			System.out.println("Enquirer com " + listaMapas[i] + "...");
+			resp = new ResponderMaze(stat, listaMapas[i]);
+			enq.connect(resp);
+			enq.discover();
+		}
+		System.out.println("----------------------------------------------------------------------------------------\n");
+	}
+
+}
