0f7fb8f7703541370b95047aa6ef4f7571ba66a3
Mateus Coelho <mateus_coelho@live.com>
2015-03-16 18:03:18 -0300

    EnquirerMaze nao esta funcionando.

--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
@@ -1,6 +1,6 @@
 package pt.c02classes.s01knowledge.s02app.actors;
 
-import java.util.Scanner;
+
 
 import pt.c02classes.s01knowledge.s01base.inter.IEnquirer;
 import pt.c02classes.s01knowledge.s01base.inter.IResponder;
@@ -14,23 +14,71 @@ public class EnquirerMaze implements IEnquirer {
 	}
 	
 	public boolean discover() {
-		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("(P)ergunta, (M)ovimento ou (F)im? ");
-		String tipo = scanner.nextLine();
-		while (!tipo.equalsIgnoreCase("F")) {
-		   System.out.print("  --> ");
-		   String pc = scanner.nextLine();
-		   switch (tipo.toUpperCase()) {
-		      case "P": String resposta = responder.ask(pc);
-		                System.out.println("  Resposta: " + resposta);
-		                break;
-		      case "M": boolean moveu = responder.move(pc);
-		                System.out.println((moveu)?"  Movimento executado!":"Não é possível mover");
-		                break;
-		   }
-			System.out.print("(P)ergunta, (M)ovimento ou (F)im? ");
-			tipo = scanner.nextLine();
+		char parede = 'D';
+		int i = 0;
+		
+		while(!responder.ask("aqui").equalsIgnoreCase("saida")) {
+			System.out.println(i);
+			i++;
+			if(parede == 'D') {
+				if(responder.ask("leste").equalsIgnoreCase("parede") && responder.ask("norte").equalsIgnoreCase("passagem"))
+					responder.move("norte");
+				else if(responder.ask("leste").equalsIgnoreCase("passagem")) {
+					responder.move("leste");
+					parede = 'B';
+				}
+				else if(responder.ask("oeste").equalsIgnoreCase("passagem")) {
+					responder.move("oeste");
+					parede = 'A';
+				}
+				responder.move("sul");
+				parede = 'E';
+			}
+			else if(parede == 'E') {
+				if(responder.ask("oeste").equalsIgnoreCase("parede") && responder.ask("sul").equalsIgnoreCase("passagem"))
+					responder.move("sul");
+				else if(responder.ask("oeste").equalsIgnoreCase("passagem")) {
+					responder.move("oeste");
+					parede = 'A';
+				}
+				else if(responder.ask("leste").equalsIgnoreCase("passagem")) {
+					responder.move("leste");
+					parede = 'B';
+				}
+				responder.move("norte");
+				parede = 'D';
+			}
+			else if(parede == 'B') {
+				if(responder.ask("sul").equalsIgnoreCase("parede") && responder.ask("leste").equalsIgnoreCase("passagem"))
+					responder.move("leste");
+				else if(responder.ask("sul").equalsIgnoreCase("passagem")) {
+					responder.move("sul");
+					parede = 'E';
+				}
+				else if(responder.ask("norte").equalsIgnoreCase("passagem")) {
+					responder.move("norte");
+					parede = 'D';
+				}
+				else if(responder.ask("sul").equalsIgnoreCase("passagem")) {
+				responder.move("oeste");
+				parede = 'A';
+				}
+			}
+			else if(parede == 'A') {
+				if(responder.ask("norte").equalsIgnoreCase("parede") && responder.ask("oeste").equalsIgnoreCase("passagem"))
+					responder.move("oeste");
+				else if(responder.ask("norte").equalsIgnoreCase("passagem")) {
+					responder.move("norte");
+					parede = 'D';
+				}
+				else if(responder.ask("sul").equalsIgnoreCase("passagem")) {
+					responder.move("sul");
+					parede = 'E';
+				}
+				responder.move("leste");
+				parede = 'B';
+			}
 		}
 		
 		if (responder.finalAnswer("cheguei"))
@@ -38,9 +86,8 @@ public class EnquirerMaze implements IEnquirer {
 		else
 			System.out.println("Fuém fuém fuém!");
 		
-		scanner.close();
-		
+
 		return true;
+		
 	}
-	
 }
